96f10e903e53dadff6c06fbf3ec782b60d5e0fc5
Fifer2 <fifer2@yandex.ru>
2019-11-23 19:31:10 +0700

    feat: render menu and filters

--- src/main.js
+++ src/main.js
@@ -3,18 +3,36 @@
 const CARDS_COUNT = 3;
 
 const createSiteMenu = () => {
-  return ``;
+  return (
+    `<nav class="trip-controls__trip-tabs  trip-tabs">
+    <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+    <a class="trip-tabs__btn" href="#">Stats</a>
+  </nav>`);
 };
 
 const createSiteFilters = () => {
-  return ``;
+  return (
+    `<form class="trip-filters" action="#" method="get">
+      <div class="trip-filters__filter">
+        <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
+        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
+      </div>
+
+      <div class="trip-filters__filter">
+        <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
+        <label class="trip-filters__filter-label" for="filter-future">Future</label>
+      </div>
+
+      <div class="trip-filters__filter">
+        <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
+        <label class="trip-filters__filter-label" for="filter-past">Past</label>
+      </div>
+
+      <button class="visually-hidden" type="submit">Accept filter</button>
+    </form>`);
 };
 
 const createBuildForm = () => {
-  return ``;
-};
-
-const createEditForm = () => {
   return ``;
 };
 
@@ -26,6 +44,13 @@ const createInfoRoute = () => {
   return ``;
 };
 
+const getTripControls = () => {
+  return document.querySelector(`.trip-controls`);
+};
+
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
+
+render(getTripControls(), createSiteMenu());
+render(getTripControls(), createSiteFilters());
