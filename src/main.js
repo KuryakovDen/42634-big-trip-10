5105841d20f97cc84f88399043c14d53d46e73e7
Fifer2 <fifer2@yandex.ru>
2019-11-23 17:33:55 +0700

    feat: add main.js to index.html

---
+++ src/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const CARDS_COUNT = 3;
+
+const createSiteMenu = () => {
+  return ``;
+};
+
+const createSiteFilters = () => {
+  return ``;
+};
+
+const createBuildForm = () => {
+  return ``;
+};
+
+const createEditForm = () => {
+  return ``;
+};
+
+const createTripCard = () => {
+  return ``;
+};
+
+const createInfoRoute = () => {
+  return ``;
+};
+
+const render = (container, template, place = `beforeend`) => {
+  container.insertAdjacentHTML(place, template);
+};
