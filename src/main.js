00d5a94714a9baee16c9b878a6a347aad3db1d5c
Fifer2 <fifer2@yandex.ru>
2019-11-24 17:47:52 +0700

    fix: remove odd strings and set true versin eslint

--- src/main.js
+++ src/main.js
@@ -435,8 +435,4 @@ render(getTripEvents(), createBuildForm());
 render(getTripEvents(), createEditEventTemplate());
 render(getTripEvents(), createTripContainerTemplate());
 
-new Array(CARDS_COUNT)
-  .fill(``)
-  .forEach(() => render(getTripEvents(), createTripDays()));
-
-
+new Array(CARDS_COUNT).fill(``).forEach(() => render(getTripEvents(), createTripDays()));
