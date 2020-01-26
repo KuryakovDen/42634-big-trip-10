57feed78022bab18d4eaabc15cfa25c52a0fa253
Fifer2 <fifer2@yandex.ru>
2020-01-26 20:09:16 +0700

    feat: added model events.js

--- src/main.js
+++ src/main.js
@@ -1,30 +1,19 @@
 import {RenderPosition, renderComponent} from './utils/render.js';
-import TripInfoComponent from './components/trip-info.js';
 import MenuComponent, {menuList} from './components/menu.js';
 import FilterComponent, {filterList} from './components/filter.js';
 import generateEventList from './mock/event-data.js';
+import Events from './models/events.js';
 
 import TripController from './controllers/trip-controller.js';
 
-const eventList = generateEventList();
-
-const sumOffers = (offerList) => offerList.reduce((accum, current) => accum + current.checked * current.cost, 0);
-const sumEvents = (events) => events.reduce((accum, current) => accum + current.cost + sumOffers(current.offers), 0);
-
 const tripMainElement = document.querySelector(`.trip-main`);
 const tripEventsElement = document.querySelector(`.trip-events`);
-const tripInfoElement = tripMainElement.querySelector(`.trip-info`);
 const tripControlElements = tripMainElement.querySelectorAll(`.trip-controls h2`);
 
-const tripController = new TripController(tripEventsElement, eventList);
-
-if (eventList.length) {
-  renderComponent(tripInfoElement, RenderPosition.AFTER_BEGIN, new TripInfoComponent(eventList));
-}
+const events = new Events(generateEventList());
+const tripController = new TripController(tripEventsElement, events);
 
 renderComponent(tripControlElements[0], RenderPosition.AFTER_END, new MenuComponent(menuList));
 renderComponent(tripControlElements[1], RenderPosition.AFTER_END, new FilterComponent(filterList));
 
 tripController.render();
-
-document.querySelector(`.trip-info__cost-value`).innerText = sumEvents(eventList);
